15e38694aafb89127877720a3eb7ca0ea8b1aa1e
Shad0ny <151963104+Shad0ny@users.noreply.github.com>
2023-11-30 16:05:57 +0100

    Create inscription.php

---
+++ inscription.php
@@ -0,0 +1,12 @@
+<?php
+$nom=$_POST['Nom']
+$prenom=$_POST['Prenom']
+$birthday=$_POST['birthday']
+$mail=$_POST['mail']
+$password2=$_POST['password2']
+$password3=$_POST['password3']
+
+$db = new SQLite3('connexion.db');
+
+$query = "INSERT INTO connexion (nom,prenom,date de naissance,addresse mail,mot de passe,confirmation du mot de passe) VALUES ('$nom','$prenom','$birthday','$mail','$password2','$password3')";
+?>
