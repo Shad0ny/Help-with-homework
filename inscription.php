6cb03504515cc14db6bd86db104bcb56f198e7fb
Shad0ny <151963104+Shad0ny@users.noreply.github.com>
2023-11-30 16:12:43 +0100

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -9,4 +9,13 @@ $password3=$_POST['password3']
 $db = new SQLite3('connexion.db');
 
 $query = "INSERT INTO connexion (nom,prenom,date de naissance,addresse mail,mot de passe,confirmation du mot de passe) VALUES ('$nom','$prenom','$birthday','$mail','$password2','$password3')";
+
+$result=$db->exec($query);
+
+if ($result) {
+    echo "Données enregistrées avec succès";
+} else {
+    echo "Erreur lors de l'enregistrement des données";
+}
+$db->close();
 ?>
